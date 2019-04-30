ffe378c0d8f89f51d7542bc94239f3b61ec435c8
Choi Doyeong <omok5674@naver.com>
2019-04-30 11:29:08 +0900

    Update 2133.py

--- 2133.py
+++ 2133.py
@@ -3,9 +3,9 @@ num = int(stdin.readline())
 countlist = [0] * 31
 countlist[2] = 3
 if num >= 4:
-	for n in range(4, num+1, 2):
-		countlist[n] = countlist[n-2]*3 + 2
-		if n-4 >= 2:
-			for n2 in range(2, n-3, 2):
-				countlist[n] += countlist[n2]*2
+    for n in range(4, num+1, 2):
+        countlist[n] = countlist[n-2]*3 + 2
+	    if n-4 >= 2:
+	        for n2 in range(2, n-3, 2):
+		    	countlist[n] += countlist[n2]*2
 print(countlist[num])
